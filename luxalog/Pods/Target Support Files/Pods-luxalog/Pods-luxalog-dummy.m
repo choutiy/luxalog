93aa12892415e4400620e647cffc2bafc6032bf0
Lidong Zhou <littlepinktea@gmail.com>
2017-02-03 12:23:50 -0800

    Adding Pods

---
+++ luxalog/Pods/Target Support Files/Pods-luxalog/Pods-luxalog-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_luxalog : NSObject
+@end
+@implementation PodsDummy_Pods_luxalog
+@end
---
+++ luxalog/Pods/Target Support Files/Pods-luxalogUITests/Pods-luxalogUITests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_luxalogUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_luxalogUITestsVersionString[];
+
